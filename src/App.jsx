67048d187f40622adfec6d43c7811f7fbc436186
Faiza <ffaizabutt01@gmail.com>
2026-07-02 22:09:43 +0900

    01-folder done

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+
+const App = () => {
+  return (
+    // <></> empty TaskSignal=(Fragments)ak fun ma ak he div askta ha is lia ham use krty han 
+<>
+<div>
+  <h1>Hello, World!</h1>
+  <p>This is a basic of React</p>
+</div><br/>
+<div>
+  <h2>This is a subheading</h2>
+</div>
+</>
+  )
+}
+
+export default App
