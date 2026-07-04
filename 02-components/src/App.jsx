afa536ef1050dc889231d86c6dd9b47f278eeafa
Faiza <ffaizabutt01@gmail.com>
2026-07-04 19:37:43 +0900

    02-folder done

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CCard from './components/CCard'
+import Navbar from './components/navbar'
+const App = () => {
+
+
+
+  return (
+  
+    <div>
+      <Navbar/>
+      <CCard/>
+    </div>  
+  )
+}
+
+export default App
---
+++ 02-components/src/components/CCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CCard = () => {
+  return (
+    <div className='CCard'>
+      <h1>Card Component</h1>
+      <p>This is a simple card component.</p>
+          </div>
+   
+  )
+}
+
+export default CCard
